bd3d5085fd3fe423d96619fee06f069cc94c80ef
yuchida-tamu <yutaside1992@gmail.com>
2022-05-01 11:05:38 +0900

    adding service mthods

--- internal/user/user.go
+++ internal/user/user.go
@@ -1,6 +1,9 @@
 package user
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 type User struct {
 	ID       string
@@ -17,4 +20,59 @@ type Store interface {
 
 type Service struct {
 	Store Store
+}
+
+func NewService(store Store) *Service {
+	return &Service{
+		Store: store,
+	}
+}
+
+func (s *Service) GetUsers(ctx context.Context) ([]User, error) {
+	users, err := s.GetUsers(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return []User{}, err
+	}
+	return users, nil
+}
+
+func (s *Service) GetUser(ctx context.Context, ID string) (User, error) {
+	user, err := s.GetUser(ctx, ID)
+	if err != nil {
+		fmt.Println(err)
+		return User{}, err
+	}
+
+	return user, nil
+}
+
+func (s *Service) PostUser(ctx context.Context, user User) (User, error) {
+	user, err := s.PostUser(ctx, user)
+	if err != nil {
+		fmt.Println(err)
+		return User{}, err
+	}
+
+	return user, nil
+}
+
+func (s *Service) UpdateUser(ctx context.Context, ID string, user User) (User, error) {
+	user, err := s.UpdateUser(ctx, ID, user)
+	if err != nil {
+		fmt.Println(err)
+		return User{}, err
+	}
+
+	return user, nil
+}
+
+func (s *Service) DeleteUser(ctx context.Context, ID string) error {
+	err := s.DeleteUser(ctx, ID)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
 }
